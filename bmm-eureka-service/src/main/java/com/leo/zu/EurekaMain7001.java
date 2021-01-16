4ae29aec47da6594e875c37330b9e723b7bff8ea
Aileopard <972635350@qq.com>
2021-01-16 20:25:15 +0800

    初始化 bmm注册中心

---
+++ bmm-eureka-service/src/main/java/com/leo/zu/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package com.leo.zu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author leo-zu
+ * @create 2021-01-16 20:17
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
